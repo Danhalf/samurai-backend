ecddeab324fb1397b6a23de98941e912206b572a
Danhalf <danhel@rocketmail.com>
2023-05-08 00:07:01 +0300

    add: users.json

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const users_json_1 = __importDefault(require("./users.json"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    let testMessage = "Current message";
-    res.send(testMessage);
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST;
+app.get('/users', (req, res) => {
+    res.send(users_json_1.default);
 });
-app.listen(port, () => {
-    console.log(`Server started on port: ${port}`);
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = users_json_1.default.find(user => user.id === Number(id));
+    res.send(user);
+});
+app.get('/find', (req, res) => {
+    const name = typeof req.query.name === 'string' ? req.query.name.toString().toLowerCase() : undefined;
+    const filteredUsers = name ? users_json_1.default.filter(user => user.name.toLowerCase().includes(name)) : null;
+    if (filteredUsers === null || filteredUsers === void 0 ? void 0 : filteredUsers.length) {
+        res.send(filteredUsers);
+    }
+    else {
+        res.send(404);
+    }
+});
+app.listen(PORT, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,36 @@
 import express, {Request, Response} from "express"
+import users from "./users.json"
 const app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST
 
-app.get('/', (req: Request, res: Response) => {
-    let testMessage = `Running on http://${HOST}:${PORT}`;
-    res.send(testMessage)
+interface IUser {
+    "id": number | string,
+    "name": string,
+    "gender": string,
+    "country": string,
+    "email": string
+}
+
+
+app.get('/users', (req: Request, res: Response) => {
+    res.send(users)
+})
+
+app.get('/users/:id', (req: Request, res: Response) => {
+    const {id} = req.params;
+    const user = users.find(user => user.id === Number(id))
+    res.send(user)
+})
+
+app.get('/find', (req: Request, res: Response) => {
+    const name: string | undefined = typeof req.query.name === 'string' ? req.query.name.toString().toLowerCase() : undefined;
+    const filteredUsers = name ? users.filter(user => user.name.toLowerCase().includes(name)) : null;
+    if (filteredUsers?.length) {
+        res.send(filteredUsers)
+    } else {
+        res.send(404)
+    }
 })
 
 app.listen(PORT, () => {
